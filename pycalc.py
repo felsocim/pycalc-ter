78d1b73cb0e5c8a31435c5e1deb848bc6a98dccb
Marek Felšöci <marek@felsoci.sk>
2026-03-05 15:26:56 +0100

    Lecture des opérandes et de l'opérateur

---
+++ pycalc.py
@@ -0,0 +1,14 @@
+# Lecture des opérandes et de l'opérateur depuis la ligne de commandes
+import sys
+
+if len(sys.argv) != 4:
+  print("Erreur : argument(s) manquant(s)", file=stderr)
+  sys.exit(1)
+
+left = float(sys.argv[1])
+operation = sys.argv[2]
+right = float(sys.argv[3])
+
+
+
+
